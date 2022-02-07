cf1f433ada697b62607d02aca53f97aaa47433aa
adamteddychang <adamchang0725@gmail.com>
2022-02-07 14:29:56 +0100

    Added book class

---
+++ book.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+
+
+class Book < item
+  attr_accessor :publisher, :cover_state 
+  def initialize(id, name, author, genre, publish_date, publisher, cover_state)
+    super(id, name, genre, author, label, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+   def can_archived? 
+    super || cover_state == "bad"
+   end
+
+
+end
