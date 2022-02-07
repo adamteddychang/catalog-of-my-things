76b4dcd1d365910194ffe31241e750f94b2d1ac3
adamteddychang <adamchang0725@gmail.com>
2022-02-07 14:30:57 +0100

    fixed linters

--- book.rb
+++ book.rb
@@ -1,16 +1,15 @@
 require_relative 'item'
 
-
 class Book < item
-  attr_accessor :publisher, :cover_state 
+  attr_accessor :publisher, :cover_state
+
   def initialize(id, name, author, genre, publish_date, publisher, cover_state)
     super(id, name, genre, author, label, publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
-   def can_archived? 
-    super || cover_state == "bad"
-   end
 
-
+  def can_archived?
+    super || cover_state == 'bad'
+  end
 end
