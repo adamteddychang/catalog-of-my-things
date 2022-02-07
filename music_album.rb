fff7066479bd11d8329451f40f8b932caf7532bc
luftedar <ugurorcun91@gmail.com>
2022-02-07 17:02:28 +0300

    Fix Linter Errors

--- music_album.rb
+++ music_album.rb
@@ -2,12 +2,13 @@ require_relative 'item'
 
 class MusicAlbum < Item
   attr_accessor :on_spotify
+
   def initialize(id, name, genre, publish_date, on_spotify)
     super(id, name, genre, label, publish_date)
     @on_spotify = on_spotify
   end
 
-  def can_be_archived?
+  def can_archived?
     super() and @on_spotify
   end
 end
