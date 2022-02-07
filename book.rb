e7fd55661b960c7c88d755b6e934b1dc867825cb
adamteddychang <adamchang0725@gmail.com>
2022-02-07 14:51:15 +0100

    fixed errors

--- book.rb
+++ book.rb
@@ -1,9 +1,9 @@
 require_relative 'item'
-class Book < item
+class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(id, name, author, genre, publish_date, publisher, cover_state)
-    super(id, name, genre, author, label, publish_date)
+  def initialize(id, name, genre, publish_date, publisher, cover_state)
+    super(id, name, genre, label, publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
