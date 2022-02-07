78dfa1d2b433ee6f63ced8f879f0ae1317ca2791
luftedar <ugurorcun91@gmail.com>
2022-02-07 15:55:25 +0300

    Fix Linters

--- music_album.rb
+++ music_album.rb
@@ -1,5 +1,7 @@
+require_relative 'item'
+
 class MusicAlbum < Item
-  def initialize(id, name, genre, author, genre, publish_date, on_spotify)
+  def initialize(id, name, author, genre, publish_date, on_spotify)
     super(id, name, genre, author, label, publish_date)
     @on_spotify = on_spotify
   end
